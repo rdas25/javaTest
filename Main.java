d7b591fc28dea8272ea3f5eb9cc47264f0d7570c
rdas25 <rdasanoor07@gmail.com>
2026-06-04 18:31:19 -0400

    Variables

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
 // Practice
+
+String firstMessage;
+int hour, minute;
+
+firstMessage = "Hello, World!";
+hours = 10, minute = 10;
+
+String language = "Java";
+
+System.out.println(firstMessage);
+System.out.print(hours + ":" + minute);
