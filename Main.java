af3093bc5fa4312587a3683198d452f2c98b970e
rdas25 <rdasanoor07@gmail.com>
2026-06-05 13:43:57 -0400

    2.6 Floating-point numbers

--- Main.java
+++ Main.java
@@ -16,5 +16,11 @@ public class Main {
         System.out.println(" Total minutes: " + (hour * 60 + minute));
 
         System.out.println(10 / 3 + " " + -10 / 3); // rounds toward zero
+
+        double floatingPoint = 10.0;
+        System.out.println(floatingPoint / 3 + " " + 10 / 3.0 + " " + floatingPoint / 3.0); // only one value needs to be a floating-point
+        
+        double zero = 1 / 3;
+        System.out.println(zero); // equals zero because the values are both integers
     }
 }
