37fa0df65200dfd2b3825a5c182bb2db8fc2efc4
rdas25 <rdasanoor07@gmail.com>
2026-06-05 14:11:03 -0400

    2.8 Operators for strings

--- Main.java
+++ Main.java
@@ -17,10 +17,15 @@ public class Main {
 
         System.out.println(10 / 3 + " " + -10 / 3); // rounds toward zero
 
-        double floatingPoint = 10.0;
+        double floatingPoint = 10.0; // decimal point is correct style but not needed
         System.out.println(floatingPoint / 3 + " " + 10 / 3.0 + " " + floatingPoint / 3.0); // only one value needs to be a floating-point
         
         double zero = 1 / 3;
         System.out.println(zero); // equals zero because the values are both integers
+
+        System.out.println(.1 * 10);
+        System.out.println(.1 + .1 + .1 + .1 + .1 + .1 + .1 + .1 + .1 + .1); // equals .9999..., to avoid rounding error use integers
+    
+        System.out.println(1 + 2 + " Hello " + 1 + 2); // left to right, the first 1 + 2 is evaluated to 3, but not the second 1 + 2
     }
 }
